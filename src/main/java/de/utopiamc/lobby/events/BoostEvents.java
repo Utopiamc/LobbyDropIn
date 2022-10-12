67daf3897c733c847f79b9e56eadb61f8d4ffe93
Leon_lp9 <leon.marcel.rabe@gmail.com>
2022-10-12 07:37:08 +0200

    Boost player when is standing on gold plate

---
+++ src/main/java/de/utopiamc/lobby/events/BoostEvents.java
@@ -0,0 +1,18 @@
+package de.utopiamc.lobby.events;
+
+import de.utopiamc.framework.api.event.Subscribe;
+import de.utopiamc.framework.api.event.qualifier.Event;
+import de.utopiamc.framework.api.stereotype.Controller;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+@Controller
+public class BoostEvents {
+
+    @Subscribe(event = PlayerMoveEvent.class)
+    public void sui(@Event PlayerMoveEvent event) {
+        if (event.getTo().getBlock().getType().getId() == 147) {
+            event.getPlayer().setVelocity(event.getPlayer().getLocation().getDirection().multiply(2));
+        }
+    }//Player will be pushed away from the pressureplate
+
+}

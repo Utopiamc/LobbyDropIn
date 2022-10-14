de0e92a3b5fabe64be2a8131f84c0ea5f43c450a
Leon_lp9 <leon.marcel.rabe@gmail.com>
2022-10-14 16:42:54 +0200

    Some Holographs Commands

--- src/main/java/de/utopiamc/lobby/Lobby.java
+++ src/main/java/de/utopiamc/lobby/Lobby.java
@@ -1,12 +1,14 @@
 package de.utopiamc.lobby;
 
 import com.google.inject.Inject;
-import de.dytanic.cloudnet.ext.bridge.node.CloudNetBridgeModule;
 import de.utopiamc.framework.api.dropin.annotations.OnDisable;
 import de.utopiamc.framework.api.dropin.annotations.OnEnable;
 import de.utopiamc.framework.api.stereotype.Plugin;
 import de.utopiamc.lobby.creator.EntityCreator;
 import de.utopiamc.lobby.creator.HoloCreator;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
 
 import java.util.logging.Logger;
@@ -33,31 +35,89 @@ public class Lobby {
         logger.info("Lobby disabled!");
     }
 
+    private String prefix = "§a§lLobby §8» §7";
+
     public void spawnHolos(){
         new HoloCreator("world", 67.5, 10.3, 107.5)
                 .addLine("§8----------")
                 .addLine("§2Internet")
                 .addLine("§8----------")
+                .onClick(player -> {
+
+                    player.sendMessage("");
+                    player.sendMessage(prefix + "§m§8---=---§9World§bWide§9Web§8§m---=---");
+                    player.sendMessage(textComponentOPEN_URL("§7Forum §8(§9§nClick§8)", "https://forum.utopiamc.de/index.php"));
+                    player.sendMessage(textComponentOPEN_URL("§7Bewerben §8(§9§nClick§8)", "https://enroll.utopiamc.de/"));
+                    player.sendMessage(textComponentOPEN_URL("§7Utopia Wert (CityBuild) §8(§9§nClick§8)", "https://wert.utopiamc.de/index.php"));
+                    player.sendMessage(prefix + "§m§8---=---§9World§bWide§9Web§8§m---=---");
+                    player.sendMessage("");
+                    player.playSound(player.getLocation(), Sound.BLOCK_SAND_BREAK,1,1);
+
+                })
                 .spawn();
 
         new HoloCreator("world", 88.5, 10.3, 107.5)
                 .addLine("§8----------")
                 .addLine("§cYou§fTube")
                 .addLine("§8----------")
+                .onClick(player -> {
+
+                    player.sendMessage("");
+                    player.sendMessage(prefix + "§m§8---=---=---§cYou§fTube§8§m---=---=---");
+                    player.sendMessage(textComponentOPEN_URL("§4Network §8| §2§lUtopiamc.de §8(§9§nClick§8)", "https://www.youtube.com/channel/UCGJ7A4T_O8fR6XpLxsJErrA"));
+                    player.sendMessage(textComponentOPEN_URL("§4Owner §8| §7Leon_lp9 §8(§9§nClick§8)", "https://www.youtube.com/channel/UCMXYILQ5PXsxzZEvakAN7lg"));
+                    player.sendMessage(textComponentOPEN_URL("§4Leitung §8| §7melonenLuca44 §8(§9§nClick§8)", "https://www.youtube.com/channel/UCMXYILQ5PXsxzZEvakAN7lg"));
+                    player.sendMessage(textComponentOPEN_URL("§4Manager §8| §7DevOskar §8(§9§nClick§8)", "https://youtube.com/channel/UCCKGKuCG9k36lX1e_cI_VpQ"));
+                    player.sendMessage(prefix + "§m§8---=---=---§cYou§fTube§8§m---=---=---");
+                    player.sendMessage("");
+                    player.playSound(player.getLocation(), Sound.BLOCK_SAND_BREAK,1,1);
+
+                })
                 .spawn();
 
         new HoloCreator("world", 90.5, 11.3, 94.5)
                 .addLine("§8----------")
                 .addLine("§eIn§csta§5gra§dm")
                 .addLine("§8----------")
+                .onClick(player -> {
+
+                    player.sendMessage("");
+                    player.sendMessage(prefix + "§m§8---=---=---§eIn§csta§5gra§dm§8§m---=---=---");
+                    player.sendMessage(textComponentOPEN_URL("§4Network §8| §2§lUtopiamc.de §8(§9§nClick§8)", "https://www.instagram.com/utopiamc.de/"));
+                    player.sendMessage(textComponentOPEN_URL("§4Owner §8| §7Leon_lp9 §8(§9§nClick§8)", "https://www.instagram.com/leon_lp9/"));
+                    player.sendMessage(textComponentOPEN_URL("§4Leitung §8| §7melonenLuca44 §8(§9§nClick§8)", "https://www.instagram.com/melonenluca44/"));
+                    player.sendMessage(prefix + "§m§8---=---=---§eIn§csta§5gra§dm§8§m---=---=---");
+                    player.sendMessage("");
+                    player.playSound(player.getLocation(), Sound.BLOCK_SAND_BREAK,1,1);
+
+                })
                 .spawn();
 
         new HoloCreator("world", 70.5, 11.3, 93.5)
                 .addLine("§8----------")
                 .addLine("§bTwitter")
                 .addLine("§8----------")
+                .onClick(player -> {
+
+                    player.sendMessage("");
+                    player.sendMessage(prefix + "§m§8---=---=---§bTwitter§8§m---=---=---");
+                    player.sendMessage(textComponentOPEN_URL("§4Network §8| §2§lUtopiamc.de §8(§9§nClick§8)", "https://twitter.com/utopiamc_de"));
+                    player.sendMessage(textComponentOPEN_URL("§4Owner §8| §7Leon_lp9 §8(§9§nClick§8)", "https://twitter.com/leon_lp9"));
+                    player.sendMessage(textComponentOPEN_URL("§4Leitung §8| §7melonenLuca44 §8(§9§nClick§8)", "https://twitter.com/melonenluca44"));
+                    player.sendMessage(textComponentOPEN_URL("§4Manager §8| §7DevOskar §8(§9§nClick§8)", "https://twitter.com/DevOskar"));
+                    player.sendMessage(prefix + "§m§8---=---=---§bTwitter§8§m---=---=---");
+                    player.sendMessage("");
+                    player.playSound(player.getLocation(), Sound.BLOCK_SAND_BREAK,1,1);
+
+                })
                 .spawn();
 
+    }
+
+    public TextComponent textComponentOPEN_URL(String text, String url) {
+        TextComponent message = new TextComponent(text);
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
+        return message;
     }
 
     public void spawnEntities(){

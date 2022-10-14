a15319d6dbb0d3386ea0322bc9b1331e24258154
Leon_lp9 <leon.marcel.rabe@gmail.com>
2022-10-14 14:47:02 +0200

    IngameShop.java

--- src/main/java/de/utopiamc/lobby/commands/IngameShopCommand.java
+++ src/main/java/de/utopiamc/lobby/commands/IngameShopCommand.java
@@ -1,27 +1,62 @@
 package de.utopiamc.lobby.commands;
 
+import com.google.inject.Inject;
 import de.utopiamc.framework.api.commands.descriptors.MapRoute;
 import de.utopiamc.framework.api.event.Subscribe;
+import de.utopiamc.framework.api.event.qualifier.Event;
+import de.utopiamc.framework.api.service.MessageService;
 import de.utopiamc.framework.api.stereotype.Command;
 import de.utopiamc.framework.api.stereotype.Controller;
+import de.utopiamc.framework.api.tasks.TaskService;
+import de.utopiamc.lobby.ingameshop.IngameShop;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.spigotmc.event.entity.EntityDismountEvent;
 
 @Controller
 @Command(value = "IngameShop")
 public class IngameShopCommand {
 
-    @MapRoute("")
-    public void ingameshop() {
+    private final TaskService taskService;
 
+    @Inject
+    public IngameShopCommand(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @MapRoute("")
+    public void ingameshop(Player player, MessageService messageService) {
+        if (player.isSneaking()) messageService.sendMessage("Du kannst den IngameShop nur offnen, wenn du nicht Sneakst!");
+        else new IngameShop(player);
     }
 
     @Subscribe(event = PlayerInteractEvent.class)
-    public void onInteract(PlayerInteractEvent event) {
+    public void onInteract(@Event PlayerInteractEvent event) {
         if (event.getClickedBlock() == null) return;
         if (event.getClickedBlock().getType() == Material.ENDER_CHEST) {
             event.getPlayer().sendMessage("§7[§6IngameShop§7] §eHallo, ich bin der IngameShop. Ich bin hier, um dir Ränge und Kisten zu verkaufen!");
             event.getPlayer().performCommand("IngameShop");
+        }
+    }
+
+    @Subscribe(event = PlayerQuitEvent.class)
+    public void onQuit(@Event PlayerQuitEvent event){
+        if(IngameShop.ingameshop.containsKey(event.getPlayer())){
+            IngameShop.ingameshop.get(event.getPlayer()).as.remove();
+            IngameShop.ingameshop.remove(event.getPlayer());
+        }
+    }
+
+    @Subscribe(event = EntityDismountEvent.class)
+    public void onSneak(@Event EntityDismountEvent event){
+        event.getEntity().sendMessage("Debug1");
+        if (event.getEntity() instanceof Player) {
+            event.getEntity().sendMessage("Debug");
+            if (IngameShop.ingameshop.containsKey((Player) event.getEntity())) {
+                IngameShop.ingameshop.get((Player) event.getEntity()).remove();
+            }
         }
     }
 
--- src/main/java/de/utopiamc/lobby/ingameshop/IngameShop.java
+++ src/main/java/de/utopiamc/lobby/ingameshop/IngameShop.java
@@ -1,4 +1,66 @@
 package de.utopiamc.lobby.ingameshop;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class IngameShop {
+
+    public static HashMap<Player, IngameShop> ingameshop = new HashMap<>();
+
+    public final Player player;
+    public final Location startLocation;
+    public final ArmorStand as;
+    public final ItemStack[] inventory;
+
+    public IngameShop(Player player){
+        this.startLocation = player.getLocation();
+        this.player = player;
+
+        ArrayList<Location> locs = new ArrayList<>();
+        locs.add(new Location(Bukkit.getWorld("world"), 58.50, 28.50, 83.50, 318, 31));
+        locs.add(new Location(Bukkit.getWorld("world"), 77.47, 15.50, 87.48, 0 ,16));
+        locs.add(new Location(Bukkit.getWorld("world"), 63.99, 50.50, 133.99, 197, 40));
+        locs.add(new Location(Bukkit.getWorld("world"), 103.50, 41.50, 181.50, 524, 18));
+        locs.add(new Location(Bukkit.getWorld("world"), 86.50, 27.50, 87.50, 28, 40));
+
+        inventory = player.getInventory().getContents();
+
+        player.getInventory().clear();
+
+        Random r = new Random();
+        int i = r.nextInt(locs.size());
+
+        Location loc = locs.get(i);
+
+        ArmorStand armorStand = (ArmorStand) Bukkit.getWorld("world").spawnEntity(loc, EntityType.ARMOR_STAND);
+
+        armorStand.setGravity(false);
+        armorStand.setSmall(true);
+        armorStand.setVisible(false);
+        armorStand.teleport(loc.add(0, -0.5, 0));
+        player.teleport(loc);
+
+        ingameshop.put(player, this);
+        this.as = armorStand;
+
+        armorStand.setPassenger(player);
+
+    }
+
+    public void remove(){
+        as.removePassenger(player);
+        player.getInventory().setContents(inventory);
+        player.teleport(startLocation);
+        as.remove();
+        ingameshop.remove(player);
+    }
+
 }
